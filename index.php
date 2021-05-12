00e26d2ebbe7a9e5c11d3377e35a1604b6116213
Sano206 <gajdos.alexander206@gmail.com>
2021-05-12 11:47:50 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+</head>
+<body>
+
+
+<div class="container">
+
+    tada
+
+</div>
+
+
+</body>
+</html>
