2de90a847780136298c6c340fc260a85d008474d
Gossky <matus.gossanyi.6@gmail.com>
2021-05-18 00:26:46 +0200

    LATEST VERSION !
    still w/o MATH!

--- questiontotest.php
+++ questiontotest.php
@@ -11,7 +11,7 @@ if (!isset($_SESSION['instructorId'])) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Pridanie otazky do testu</title>
+    <title>Question Maker</title>
     <link rel="stylesheet" href="CSS/custom.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
